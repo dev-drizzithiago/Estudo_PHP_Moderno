6cbab318faf4a1435406283e552cda88f870fbe1
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-08-09 20:14:55 -0300

    Update cad.php

--- ex004/cad.php
+++ ex004/cad.php
@@ -4,17 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
         <h1>Resultado do processamento</h1>
     </header>
     <main>
+        <p>
         <?php 
             $nome = assert($_GET["nome"])?$_GET["nome"]:"<desconhecido>";
             $sobrenome = assert($_GET["sobrenome"])?$_GET["sobrenome"]:"<desconhecido>";
-
+            echo "$nome $sobrenome"
         ?>
+        </p>
     </main>
 </body>
 </html>
