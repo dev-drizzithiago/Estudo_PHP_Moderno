8a036fadd617e5ea25b1af0192759d857f268d55
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-08-09 20:21:24 -0300

    Update cad.php

--- ex004/cad.php
+++ ex004/cad.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Resultado do processamento</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -19,5 +19,6 @@
         ?>
         </p>
     </main>
+    <a href="./index.html">Voltar</a>    
 </body>
 </html>
