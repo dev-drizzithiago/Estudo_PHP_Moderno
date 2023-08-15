2cc37bd4729e4059194723911631f1de2de35ebf
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-08-14 22:04:37 -0300

    Create calcu.php

---
+++ Desafios/Antecessor/Sucessor/calcu.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculando os valores</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php 
+     $valor_num = $_GET["numero"] ?? 0;
+
+    
+    ?>    
+</body>
+</html>
