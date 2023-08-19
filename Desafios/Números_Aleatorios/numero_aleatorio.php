73625683d954377f905579ddd486d4278e526a71
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-08-19 19:16:05 -0300

    Create numero_aleatorio.php

---
+++ Desafios/Números_Aleatorios/numero_aleatorio.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>GERADOR DE NÚMEROS ALEATORIOS!!</title>
+</head>
+<body>
+    <h1>GERADOR DE NÚMEROS ALEATORIOS!!</h1>
+    <?php
+        $numero_gerado = rand(0,100);
+        echo "<p>Número gerado foi... $numero_gerado </p>";        
+    ?>
+    <form action="index.html">
+    <input type="submit" value="Voltar">
+    </form>
+</body>
+</html>
