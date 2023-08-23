f5ce60b658dc06af8a787947e5253f5510656231
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-08-23 20:20:27 -0300

    Update conversor.php

--- Desafios/Conversor de moedas/conversor.php
+++ Desafios/Conversor de moedas/conversor.php
@@ -2,6 +2,13 @@
 <html leng="pt-br">
     <head>
         <meta charset="utf-8">
-        <title>Convertendo Moedas</title>
+        <title>Convertendo Real em Dolar</title>
     </head>
+<body>
+       <?php
+            $valor_real = (float) $_GET["moeda"] ?? '0';            
+            $valor_usa = $valor_real / 4.86;
+            echo "O valor de R$ $valor_real em U$ $valor_usa";
+       ?>
+</body>
 </html>
