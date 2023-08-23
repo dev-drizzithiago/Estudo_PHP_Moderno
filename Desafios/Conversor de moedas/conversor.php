4367d0341cc42b815eb221fcd68f42dc74fe34cd
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-08-23 20:56:49 -0300

    Update conversor.php

--- Desafios/Conversor de moedas/conversor.php
+++ Desafios/Conversor de moedas/conversor.php
@@ -8,7 +8,12 @@
        <?php
             $valor_real = (float) $_GET["moeda"] ?? '0';            
             $valor_usa = $valor_real / 4.86;
-            echo "O valor de R$ $valor_real em U$ $valor_usa";
-       ?>
+
+            
+            echo "<p align='center'> O valor de R$ $valor_real em U$ $valor_usa </p>";       
+        ?>
+        </br>
+        <p align="center"><a href="index.html" id="idbotao">Voltar</a></p>
+        <p align="center"><a href="https://www.google.com/finance/quote/USD-BRL?sa=X&ved=2ahUKEwjW65Sr9vOAAxVEq5UCHdzDBGUQmY0JegQIBhAr" target="self">Fonte</a></p>
 </body>
 </html>
