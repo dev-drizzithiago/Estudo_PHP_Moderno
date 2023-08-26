4af2c78a274fcfb15499c201eb19d390c24575b6
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-08-26 11:57:11 -0300

    Update numero_aleatorio.php

--- Desafios/Números_Aleatorios/numero_aleatorio.php
+++ Desafios/Números_Aleatorios/numero_aleatorio.php
@@ -7,13 +7,15 @@
     <title>GERADOR DE NÚMEROS ALEATORIOS!!</title>
 </head>
 <body>
+    <main>
     <h1>GERADOR DE NÚMEROS ALEATORIOS!!</h1>
     <?php
         $numero_gerado = rand(0,100);
         echo "<p>Número gerado foi... $numero_gerado </p>";        
     ?>
-    <form action="index.html">
-    <input type="submit" value="Voltar">
-    </form>
+        <form action="index.html">
+         <input type="submit" value="Voltar">
+        </form>
+    </main>
 </body>
 </html> 
