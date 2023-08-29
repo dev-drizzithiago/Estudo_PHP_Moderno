893bf0a367b7b25f3a7c3c8c19f69fd70019bd00
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-08-28 21:12:19 -0300

    Create calc.php

---
+++ Desafios/01-AntecessoSucessor/calc.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Antecessor/Sucessor</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+    <body>
+        <main>   
+            <h1>Resultado</h1>
+            <?php 
+                $valor = $_GET["numero"] ?? 0;
+                $antes = $valor - 1;
+                $depois = $valor + 1;
+                echo "O antecessor de $valor é $antes </br>";
+                echo "O sucessor de $valor é $depois </br>";
+            ?>
+            <a href="index.html">Voltar</a>
+        </main>
+    </body>
+</html>
