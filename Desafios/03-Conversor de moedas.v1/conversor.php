49ddbdb27a62eabd4b0833518416e8608e6da82e
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-08-28 21:12:35 -0300

    Create conversor.php

---
+++ Desafios/03-Conversor de moedas.v1/conversor.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html leng="pt-br">
+<head>
+    <meta charset="utf-8">
+    <title>Convertendo Real em Dolar</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<main>
+        <h1> Convertendo REAL EM DOLAR </h1>
+        <?php
+            $valor_real = $_GET["moeda"] ?? 0;
+            $valor_base = 4.87;
+            $valor_usa = $valor_real / $valor_base;  
+            echo "Valor base do dia \$$valor_base ";       
+            echo "<p class='eco'> O valor de R$ ". number_format($valor_real, 2, ",", "."). " em U$". number_format($valor_usa, 2, ',', '.'). "</p>";
+        ?>
+        </br>
+        <button onclick="javascript:window.location.href='index.html'">Voltar</button>
+        <a href="https://www.google.com/finance/quote/USD-BRL?sa=X&ved=2ahUKEwjW65Sr9vOAAxVEq5UCHdzDBGUQmY0JegQIBhAr" target="_blank">FONTE</a>       
+</main>
+</body>
+</html>
