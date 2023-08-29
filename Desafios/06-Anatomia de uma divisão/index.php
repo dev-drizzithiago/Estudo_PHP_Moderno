07a83e0eabc5d187b73a4c82fe5136064ddad390
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-08-28 21:25:04 -0300

    Update index.php

--- Desafios/06-Anatomia de uma divisão/index.php
+++ Desafios/06-Anatomia de uma divisão/index.php
@@ -7,12 +7,16 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <?php 
+        $dividendo = $_SERVER["num_1"] ?? 0;
+        $divisor = $_SERVER["num_2"] ?? 0;
+    ?>
     <main>
-        <form action="" method="post">
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
             <label for="dividendo">Dividendo</label>
-            <input type="number" name="num_1" id="num_1" require>
+            <input type="number" name="num_1" id="num_1" require value="<?=$dividendo?>">
             <label for="divisor">Dividor</label>
-            <input type="number" name="num_2" id="num_2" require>
+            <input type="number" name="num_2" id="num_2" require value="<?=$divisor?>">
             <input type="submit" value="Analisar">
         </form>
     </main>
