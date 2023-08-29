93eea563a105ffdb3cdba1a5af13afbb139c3cc7
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-08-28 21:19:38 -0300

    Update index.php

--- Desafios/06-Anatomia de uma divisão/index.php
+++ Desafios/06-Anatomia de uma divisão/index.php
@@ -7,6 +7,14 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <main>
+        <form action="" method="post">
+            <label for="dividendo">Dividendo</label>
+            <input type="number" name="num_1" id="num_1" require>
+            <label for="divisor">Dividor</label>
+            <input type="number" name="num_2" id="num_2" require>
+            <input type="submit" value="Analisar">
+        </form>
+    </main>
 </body>
 </html>
