22cb7fe13031f5147c98ae85a79c2c7d0f2c6d80
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-08-28 21:29:50 -0300

    Update index.php

--- Desafios/06-Anatomia de uma divisão/index.php
+++ Desafios/06-Anatomia de uma divisão/index.php
@@ -8,17 +8,21 @@
 </head>
 <body>
     <?php 
-        $dividendo = $_SERVER["num_1"] ?? 0;
-        $divisor = $_SERVER["num_2"] ?? 0;
+        $dividendo = $_SERVER["num_1"] ?? 1;
+        $divisor = $_SERVER["num_2"] ?? 1;        
     ?>
     <main>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
             <label for="dividendo">Dividendo</label>
             <input type="number" name="num_1" id="num_1" require value="<?=$dividendo?>">
-            <label for="divisor">Dividor</label>
+            <label for="divisor">Divisor</label>
             <input type="number" name="num_2" id="num_2" require value="<?=$divisor?>">
             <input type="submit" value="Analisar">
         </form>
+    <?php 
+        $resultado = $dividendo / $divisor;
+        echo "O resultado da divisão é $resultado";
+    ?>
     </main>
 </body>
 </html>
