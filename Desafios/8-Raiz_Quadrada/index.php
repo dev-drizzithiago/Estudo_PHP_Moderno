1f2cfd27f7230cbb14cea6dae7b579a353fe557b
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-08-30 20:45:15 -0300

    Create index.php

---
+++ Desafios/8-Raiz_Quadrada/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculo de Raiz</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <form action="$_SERVER['']" method="get"></form>
+
+    </main>    
+</body>
+</html>
