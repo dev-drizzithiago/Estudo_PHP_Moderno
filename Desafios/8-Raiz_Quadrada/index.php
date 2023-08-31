fd82a61d30c004725666c3c59b35f9d28c89b57b
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-08-30 21:11:23 -0300

    Update index.php

--- Desafios/8-Raiz_Quadrada/index.php
+++ Desafios/8-Raiz_Quadrada/index.php
@@ -7,10 +7,20 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <?php 
+        $valor_raiz = (int) $_REQUEST["_raiz_"] ?? 0;
+        $raiz_x_2 = $valor_raiz ** (1/2);
+        $raiz_x_3 = $valor_raiz ** (1/3);
+    ?>
     <main>
         <form action="<?=$_SERVER['SCRIPT_NAME']?>" method="get">
-    
+        <label for="_raiz">Número </label>
+        <input type="number" name="_raiz_" id="_raiz_" require value="<?=$valor_raiz?>">
+        <input type="submit" value="Calcular RAÍZE">
     </form>
-    </main>    
+        <h1>Analisando o número <?=$valor_raiz?></h1>
+        <?="<p>A sua raiz <strong>QUADRADA</strong> é <i>". number_format($raiz_x_2, 3). "</i></p>"?>
+        <?="<p>A sua raiz <strong>CÚBICA</strong> é <i>". number_format($raiz_x_3, 3). "</i></p>"?>
+    </main> 
 </body>
 </html>
