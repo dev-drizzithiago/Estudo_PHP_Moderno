4af316b90b61228fef1dbe2de0ad0552e7a8de41
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-09-03 09:47:44 -0300

    Update index.php

--- Desafios/10-Calculando_Idade/index.php
+++ Desafios/10-Calculando_Idade/index.php
@@ -7,24 +7,24 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php 
-        $_ano_nasc = $_REQUEST["ano_nasc"] ?? 1900;
-        $_ano_atual = $_REQUEST["ano_atual"] ?? 2023;
+    <?php
+        $_ano_atual_sistema = date("Y");
+        $_ano_nasc = $_GET["ano_nasc"] ?? 1900;
+        $_ano_atual = $_GET["ano_atual"] ?? $_ano_atual_sistema;
         $_calc_idade = $_ano_atual - $_ano_nasc;           
     ?>
     <main>
         <h1>Calculando a sua idade!</h1>
-        <form action="" method="$_GET">
+        <form action="<?=$_SERVER["SCRIPT_NAME"]?>" method="$_GET">
             <label for="ano_nasc">Em que ano você nasceu?</label>
-            <input type="number" name="ano" id="ano">
+            <input type="number" name="ano_nasc" id="ano_nasc" value="<?=$_ano_nasc?>">
             <label for="ano_atual">Quer saber sua idade em qual ano? (Estamos em 2023)</label>
-            <input type="number" name="ano_atual" id="ano_atual">
+            <input type="number" name="ano_atual" id="ano_atual" value="<?=$_ano_atual_sistema?>">
             <input type="submit" value="Qual sera minha idade?">
-        </form>
-    
+        </form>    
     <h1>Resultado!</h1>
     <?php 
-        echo "<p>Quem nasceu no ano de $_ano_nasc vai ter $_calc_idade em $_ano_atual</p>";
+        echo "<p>Quem nasceu no ano de <strong><i>$_ano_nasc</i></strong> vai ter $_calc_idade em <strong>$_ano_atual</strong></p>";
     ?>
     </main>    
 </body>
