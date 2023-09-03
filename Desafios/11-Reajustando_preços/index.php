18ef5c1cef1a6b85b11cfd7f5b36295d03b753af
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-09-03 10:17:28 -0300

    Update index.php

--- Desafios/11-Reajustando_preços/index.php
+++ Desafios/11-Reajustando_preços/index.php
@@ -7,11 +7,17 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <?php 
+        $_valor_preco = $_REQUEST["preco"] ?? 0;
+        $_valor_percentagem = $_REQUEST["porcentagem"] ?? 0;
+    
+    ?>
     <main>
         <form action="" method="get">
         <label for="preco">Preço do produto(R$)</label>
         <input type="number" name="preco" id="preco">
         <label for="porcentual">Qual será o percentual de rajuste?</label>
+        <input type="number" name="porcentagem" id="porcentagem">
         </form>
     </main>    
 </body>
