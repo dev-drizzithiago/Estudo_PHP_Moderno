1272b8960868d3cdbd0c6691f037077252a81ce5
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-09-03 09:56:23 -0300

    Create index.php

---
+++ Desafios/11-Reajustando_preços/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reajustando Preços</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <form action="" method="get">
+        <label for="preco">Preço do produto(R$)</label>
+        <input type="number" name="preco" id="preco">
+        <label for="porcentual">Qual será o percentual de rajuste?</label>
+        </form>
+    </main>    
+</body>
+</html>
