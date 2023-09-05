37ca067ea7a091210c1b5cdfb20569274daca301
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-09-04 21:24:03 -0300

    Update index.php

--- Desafios/13-Caixa_eletronico/index.php
+++ Desafios/13-Caixa_eletronico/index.php
@@ -6,14 +6,26 @@
     <title>Caixa Eletronico</title>
     <link rel="stylesheet" href="style.css">
 </head>
+<?php 
+    //notas disponiveis
+    $_100 = 5;
+    $_50 = 2;
+    $_10 = 10;
+    $_5 = 5;
+
+    // Valor do saque
+    $_valor_saque = $_REQUEST["moeda"] ?? 0.00;
+
+?>
 <body>
-    <h1>Caixa eletronico</h1>
-    <main>        
+    <main>
+    <h1 align="center">Caixa eletronico</h1>
         <form action="<?=$_SERVER["SCRIPT_NAME"]?>" method="get">
         <label for="caixa">Qual valor deseja sacar? (R$)</label>
         <input type="number" name="moeda" id="moeda">
         <input type="submit" value="Sacar">
     </form>
-    </main>    
+    <h2 align="center">Saque de R$<?=$_valor_saque?> realizado</h2>
+    </main>
 </body>
 </html>
