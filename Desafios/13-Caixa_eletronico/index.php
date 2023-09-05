742d1d2d6553fe921701cca37af5a289181e84b5
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-09-04 21:38:33 -0300

    Update index.php

--- Desafios/13-Caixa_eletronico/index.php
+++ Desafios/13-Caixa_eletronico/index.php
@@ -8,14 +8,15 @@
 </head>
 <?php 
     //notas disponiveis
+    $_200 = 2;
     $_100 = 5;
     $_50 = 2;
+    $_20 = 4;
     $_10 = 10;
     $_5 = 5;
 
     // Valor do saque
     $_valor_saque = $_REQUEST["moeda"] ?? 0.00;
-
 ?>
 <body>
     <main>
@@ -26,6 +27,14 @@
         <input type="submit" value="Sacar">
     </form>
     <h2 align="center">Saque de R$<?=$_valor_saque?> realizado</h2>
+        <ol type="disc">
+            <li><a href="./img/_5_reais.jpg"></a></li>
+            <li><a href="./img/_10reais.jpg"></a></li>
+            <li><a href="./img/_20_reais.jpg"></a></li>
+            <li><a href="./img/_50_reais.jpg"></a></li>
+            <li><a href="./img/_100_reais.jpg"></a></li>
+            <li><a href="./img/_200_reais.jpg"></a></li>
+        </ol>
     </main>
 </body>
 </html>
