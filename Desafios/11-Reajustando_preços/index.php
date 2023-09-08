d0d8bcc8fde1438d6cae6f4c580fbb06165f5da0
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-09-08 20:47:38 -0300

    Update index.php

--- Desafios/11-Reajustando_preços/index.php
+++ Desafios/11-Reajustando_preços/index.php
@@ -15,20 +15,21 @@
     <main>
         <form action="<?=$_SERVER["SCRIPT_NAME"]?>" method="get">
         <label for="preco">Preço do produto(R$)</label>
-        <input type="number" name="preco" id="preco" value="<?=$_valor_preco?>">
+        <input type="number" name="preco" id="preco" step="0.01" value="<?=$_valor_preco?>">
         <label for='reaj'>Qual será o percentual de rajuste?(<strong><span id="porc">?</span>%</strong>)</label>
-        <input type="range" name="reaj" id="reaj" min="1" max="100" step="1" oninput="mudaValor()">
+        <input type="range" name="reaj" id="reaj" min="0" max="100" step="1" oninput="mudaValor()">
         <input type="submit" value="Reajustar">
         </form>
         <h1>Resultado do Reajuste!</h1>
     <?php 
+        // Depois vou colocar botão de radio para poder escolher se quer dar desconto ou aumentar o valor
         echo "O produto custava R$<strong>". number_format($_valor_preco, 2, ",", "."). "</strong>, com <i><strong>$_valor_percentagem%</strong></i> de aumento vai passar a custar R$<strong>". number_format($_valor_reajuste, 2, ",", "."). "</strong> a partir de agora.";       
     ?>
     </main>
     <script>
          mudaValor();
         function mudaValor() {
-            p.innerText = reaj.value;
+            porc.innerText = reaj.value;
         }
     </script>
 </body>
