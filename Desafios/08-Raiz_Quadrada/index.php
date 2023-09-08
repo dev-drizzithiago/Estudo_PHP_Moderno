a5ce37b896eb8bdbbae0fbb6375bc026066d6143
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-09-08 19:08:54 -0300

    Update index.php

--- Desafios/08-Raiz_Quadrada/index.php
+++ Desafios/08-Raiz_Quadrada/index.php
@@ -7,6 +7,10 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <!--Media simples é quando os dois pontos se encontram no meio, ex: 10 e 20, o meio passa a ser 14-->
+    <!--Media ponderada é quando os dois pontos se encontram, mas não exatamente no meio, depende do pesa de cada pontos, ex:
+o ponto "A" esta no ponto 6 e o ponto "B" esta no ponto 21, o ponto "A" possui peso 2 e o ponto "B" possui peso 1, quando o ponto "A" puxa 2 números, enquanto o ponto "B" puxa apenas 1 número, os dois se encontraram quando o número for 11-->
+    <!--Basta usar esse código e usar esses resultados para tirar a prova-->
     <?php 
         $valor_raiz = $_REQUEST["_raiz_"] ?? 0;
         $raiz_x_2 = $valor_raiz ** (1/2);
